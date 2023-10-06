d313298bc101c5e5026b81ab2355ef95cbe8ef34
MohamedYashar <arrafathyas@gmail.com>
2023-10-06 17:19:28 +0530

    v2

--- db.config.js
+++ db.config.js
@@ -5,12 +5,12 @@ const { default: mongoose } = require("mongoose");
 
 async function init (){
 
-    let URL = `${process.env.MongoDB_url}/${process.env.DB_name}`
-    if(process.env.Node_Environment ==="production"){
-        URL= `mongodb+srv://${process.env.MongoDB_username}:${ process.env.MongoDB_password}@cluster0.iffbd.mongodb.net/${process.env.DB_name}`
+    let URI = `${process.env.MongoDB_url}/${process.env.DB_name}`;
+    if(process.env.Node_Environment === "production"){
+        URI= `mongodb+srv://${process.env.MongoDB_username}:${ process.env.MongoDB_password}@cluster0.iffbd.mongodb.net/${process.env.DB_name}`
     }
     try {
-        await mongoose.connect(`${process.env.MongoDB_url}/${process.env.DB_name}`);
+        await mongoose.connect(URI);
         console.log( "MONGO_DB IS CONNECTED TO HTTP-SERVER  ")
      } catch (error) {
          console.log( "Error", error)
